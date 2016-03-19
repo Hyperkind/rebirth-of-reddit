816fe18aa7e5430670434f32cf23768809d7c2f5
Jon Ah-Nee <jonahnee@gmail.com>
2016-03-19 09:26:49 -1000

    things dont break

--- js/app.js
+++ js/app.js
@@ -4,17 +4,73 @@
   *postBox
 */
 
-var PostBox = React.createClass({
+var PostHolder = React.createClass({
+
+  getInitialState: function() {
+    return { data: [] };
+  },
+
+  loadPostsFromSite: function() {
+    var _this = this;
+    $.ajax({
+      url: this.props.url,
+      method: 'GET',
+      datatype: 'json',
+      success: function(data) {
+        _this.setState({ data: data.data.children })
+        console.log(_this.state.data);
+      }
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadPostsFromSite();
+  },
+
   render: function() {
     return (
-      <div className="PostBox">
+      <div className="postHolder">
+        <PostList data={this.state.data} />
         Why hello there!
+      </div>
+    );
+  }
+
+});
+
+var PostList = React.createClass({
+
+  render: function() {
+    var postNodes = this.props.data.map(function(post, index) {
+      return (
+        <Post key={index} title={post.data.title}></Post>
+
+      );
+    })
+    return (
+      <div className="postList">
+        {postNodes}
+      </div>
+    );
+  }
+
+});
+
+var Post = React.createClass({
+
+  render: function() {
+    return(
+      <div className="post">
+        <h2 className="postTitle">
+          {this.props.author}
+        </h2>
       </div>
     )
   }
+
 });
 
 ReactDOM.render(
-  <PostBox />,
+  <PostHolder url="https://www.reddit.com/r/leagueoflegends.json" />,
   document.getElementById('content')
 );
