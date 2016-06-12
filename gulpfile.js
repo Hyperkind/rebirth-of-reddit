37550dce38ffdd244110b17cb8993302a8812aa8
Jon Ah-Nee <jonahnee@gmail.com>
2016-06-11 15:42:00 -1000

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
   console.log('why hello there');
 })
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
